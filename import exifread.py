427cfbf5f8e2385ac1f200ab1b5a38ce6ab795d3
adelmassimo <adel.massimo.ramadan@gmail.com>
2017-09-17 11:42:29 +0200

    Porca figa

--- import exifread.py
+++ import exifread.py
@@ -13,33 +13,12 @@ file = "GalaxyS5.m4a"
 # 	metadata = et.get_metadata_batch('/User/adel/Desktop/Samples/GalaxyS5.m4a')
 
 files = ["Samples/GalaxyS5.m4a", "Samples/andre.mp3"]
-files = ["Samples/GalaxyS5.m4a"]
+#files = ["Samples/GalaxyS5.m4a"]
+
 with exiftool.ExifTool() as et:
-    allMetadata = et.get_metadata_batch(files)
+    allMetadatas = et.get_metadata_batch(files)
     #print metadata
-for metadata in allMetadata:
+for metadata in allMetadatas:
 	#FORMATO QUERY MYSQL: INSERT INTO myDb.myTable('','') VALUES ('', '')
-	queryHead, queryValues = "INSERT INTO pela.users(", ") VALUES ("
-	firstTurn = 1
- 	for key in metadata:
- 		friendlyKey = key.split(":")[-1]
- 		data = str(metadata[key])
- 		if firstTurn:
-	 		queryHead += friendlyKey 
-	 		queryValues += data
-	 		firstTurn = 0
-	 	else:
-	 		queryHead += "," + friendlyKey 
-	 		queryValues += "," + data
-
- 	
- 	query = queryHead + queryValues + ")"
- 	#print query
- 	query = "ALTER TABLE pela.users ADD COLUMN sa VARCHAR(255)"
- 	sql.runQuery(query)
- 		#print friendlyKey+": "+str(metadata[key])
-	print '________________________________'
-
-    #print("{:20.20} {:20.20}".format(d["SourceFile"]))
-
-
+	
+	sql.insertFromDic(metadata)
--- sqlConnection.py
+++ sqlConnection.py
@@ -1,22 +1,57 @@
 import pymysql
 
-def runQuery(query):
-    connection = pymysql.connect(host='127.0.0.1',
-                                 user='root',
-                                 password='root',
-                                 db='pela',
-                                 charset='utf8mb4',
-                                 port=8889,
-                                 cursorclass=pymysql.cursors.DictCursor)
+def insertFromDic(dictionary):
+    connection = pymysql.connect(host='127.0.0.1',user='root',password='root',db='pela',charset='utf8mb4',port=8889,cursorclass=pymysql.cursors.DictCursor)
+    
+    queryHead, queryValues = "INSERT INTO `dataset`(`id`", ") VALUES (NULL"
+    for key in dictionary:
+        
+        friendlyKey = key.split(":")[-1]
+        data = str(dictionary[key])
+        
+        updateTables(friendlyKey)
 
+        queryHead += ",`" + friendlyKey.replace("`", '"') + "`"
+        queryValues += ",'" + data.replace("'", '"') + "'"
+
+    query = queryHead + queryValues + ")"
+    print query
     try:
         with connection.cursor() as cursor:
-            # Create a new record
-            
             cursor.execute(query)
+       connection.commit()
+    finally:
+        connection.close()
 
-        # connection is not autocommit by default. So you must commit to save
-        # your changes.
+#this function get a Filed as parameter and, if it's a never seen Metadata, tables are updated to contain it.
+def updateTables(field):
+    connection = pymysql.connect(host='127.0.0.1',user='root',password='root',db='pela',charset='utf8mb4',port=8889,cursorclass=pymysql.cursors.DictCursor)
+    #this query check if there is an occurrence of the field
+    query = "SELECT COUNT(Name) as 'occurrencies' FROM knownMetadata WHERE Name = '"+field+"'"
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            result = cursor.fetchone()
+            #if there is not...
+            if result['occurrencies'] == 0:
+                #add the metadata to the kowledge
+                query = "INSERT INTO `knownMetadata` (`id`, `Name`) VALUES (NULL, '"+field+"')"
+                cursor.execute(query)
+                #and add a column on the dataset table
+                query = "ALTER TABLE `dataset` ADD `"+field+"`  VARCHAR(255) NULL DEFAULT NULL AFTER `id`;"
+                cursor.execute(query)
         connection.commit()
     finally:
         connection.close()
+
+
+
+
+
+
+
+
+
+
+
+
