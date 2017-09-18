9e2cc92e17007c2e45de347d57456c16e354a957
adelmassimo <adel.massimo.ramadan@gmail.com>
2017-09-18 14:52:08 +0200

    vai pela

--- import exifread.py
+++ import exifread.py
@@ -1,18 +1,11 @@
 #http://pymysql.readthedocs.io/en/latest/user/examples.html
 import sqlConnection as sql
 import exiftool
-
-
-#help(exiftool)
-
-path = "Samples/"
-file = "GalaxyS5.m4a"
-
 # with exiftool.ExifTool() as et:
 # 	print path+file
 # 	metadata = et.get_metadata_batch('/User/adel/Desktop/Samples/GalaxyS5.m4a')
-
-files = ["Samples/GalaxyS5.m4a", "Samples/andre.mp3"]
+deviceModel = 'randomDownload.mp3'
+files = ["Samples/"+deviceModel]
 #files = ["Samples/GalaxyS5.m4a"]
 
 with exiftool.ExifTool() as et:
@@ -20,5 +13,5 @@ with exiftool.ExifTool() as et:
     #print metadata
 for metadata in allMetadatas:
 	#FORMATO QUERY MYSQL: INSERT INTO myDb.myTable('','') VALUES ('', '')
-	
+	metadata['groundtruth'] = deviceModel.split('.')[0]
 	sql.insertFromDic(metadata)
--- sqlConnection.py
+++ sqlConnection.py
@@ -1,7 +1,11 @@
 import pymysql
-
+database = 'forensicsAnalysis'
+user = 'root'
+pw = 'root'
+host = 'localhost'
+port = 8889
 def insertFromDic(dictionary):
-    connection = pymysql.connect(host='127.0.0.1',user='root',password='root',db='pela',charset='utf8mb4',port=8889,cursorclass=pymysql.cursors.DictCursor)
+    connection = pymysql.connect(host=host,user=user,password=pw,db=database,charset='utf8mb4',port=port,cursorclass=pymysql.cursors.DictCursor)
     
     queryHead, queryValues = "INSERT INTO `dataset`(`id`", ") VALUES (NULL"
     for key in dictionary:
@@ -15,17 +19,16 @@ def insertFromDic(dictionary):
         queryValues += ",'" + data.replace("'", '"') + "'"
 
     query = queryHead + queryValues + ")"
-    print query
     try:
         with connection.cursor() as cursor:
             cursor.execute(query)
-       connection.commit()
+        connection.commit()
     finally:
         connection.close()
 
 #this function get a Filed as parameter and, if it's a never seen Metadata, tables are updated to contain it.
 def updateTables(field):
-    connection = pymysql.connect(host='127.0.0.1',user='root',password='root',db='pela',charset='utf8mb4',port=8889,cursorclass=pymysql.cursors.DictCursor)
+    connection = pymysql.connect(host=host,user=user,password=pw,db=database,charset='utf8mb4',port=port,cursorclass=pymysql.cursors.DictCursor)
     #this query check if there is an occurrence of the field
     query = "SELECT COUNT(Name) as 'occurrencies' FROM knownMetadata WHERE Name = '"+field+"'"
     try:
@@ -35,7 +38,7 @@ def updateTables(field):
             #if there is not...
             if result['occurrencies'] == 0:
                 #add the metadata to the kowledge
-                query = "INSERT INTO `knownMetadata` (`id`, `Name`) VALUES (NULL, '"+field+"')"
+                query = "INSERT INTO `knownMetadata` (`id`, `name`) VALUES (NULL, '"+field+"')"
                 cursor.execute(query)
                 #and add a column on the dataset table
                 query = "ALTER TABLE `dataset` ADD `"+field+"`  VARCHAR(255) NULL DEFAULT NULL AFTER `id`;"
