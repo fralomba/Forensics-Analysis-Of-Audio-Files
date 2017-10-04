0a93e392e3f4e81638b9dad60bc85947a6d678ae
fralomba <franci.lomba23894@gmail.com>
2017-10-04 22:41:06 +0200

    fgh

--- webapp/compareResult.php
+++ webapp/compareResult.php
@@ -6,33 +6,40 @@
 define("UPLOAD_DIR", "uploads/");
 if(isset($_POST['action']) and $_POST['action'] == 'upload')
 {
-    if(isset($_FILES['user_file']))
+    if(isset($_FILES['user_file1']) and isset($_FILES['user_file1']))
     {
-        $file = $_FILES['user_file'];
-        echo "<h2>".$file['name']."</h2>";
-        if($file['error'] == UPLOAD_ERR_OK and is_uploaded_file($file['tmp_name']))
+        $file1 = $_FILES['user_file1'];
+        $file2 = $_FILES['user_file2'];
+
+        echo "<h2>".$file1['name']."</h2>";
+        echo "<h2>".$file2['name']."</h2>";
+
+        if($file1['error'] == UPLOAD_ERR_OK and is_uploaded_file($file1['tmp_name']))
         {
-            move_uploaded_file($file['tmp_name'], UPLOAD_DIR.$file['name']);
+            move_uploaded_file($file1['tmp_name'], UPLOAD_DIR.$file1['name']);
+            move_uploaded_file($file2['tmp_name'], UPLOAD_DIR.$file2['name']);
         }
     }
 }
 
-
 $command = 'export LC_ALL=en_US.UTF-8
 		 export LANG=en_US.UTF-8
-		 python ../compare.py '.UPLOAD_DIR.$file['name'];
+         python ../compare.py '.UPLOAD_DIR.$file1['name'].' '.UPLOAD_DIR.$file2['name'];
 
 $output = shell_exec($command);
 
 if($output != NULL)
-echo "<script>var datas =".$output.";</script>";
+echo "<script>var datas;datas =".$output.";</script>";
 else{
 	echo "<h1>Something Wrong<h1>";
 	exit;}
 
 ?>
 
-<script src="http://d3js.org/d3.v3.min.js"></script>
-<script src="js/chartPlotter.js"></script>
+<button class="container" onmouseover="printDatas()"><a href="/webapp/index.php">BACK</a></button>
 
-<button class="container"> BACK </button>
+<script type="text/javascript">
+    function printDatas(){
+        alert(datas);
+    }
+</script>
--- webapp/comparison.php
+++ webapp/comparison.php
@@ -2,28 +2,23 @@
 
 <html>
     <head>
-        <title>File Comparison</title>
+        <title>File upload</title>
     </head>
     <body>
         <center>
-            <h1>Select the files you want to compare:</h1> 
-            <div class="container" id="leftBox">
+            <h1>1 vs 1</h1> 
+            <h3>Compare</h3>
+            <div class="container">
                 <form method="post" action="compareResult.php" enctype="multipart/form-data">
                     <input type="hidden" name="action" value="upload" />
-                    <label>Load native audio file:</label>
-                    <input type="file" name="user_file" />
+                    <label>Carica il tuo file1:</label>
+                    <input type="file" name="user_file1" />
+                    <label>Carica il tuo file2:</label>
+                    <input type="file" name="user_file2" />
+                    <br />
+                    <input type="submit" value="Compare" />
                 </form>
             </div>
-            <div class="container" id="rightBox">
-                <form method="post" action="compareResult.php" enctype="multipart/form-data">
-                    <input type="hidden" name="action" value="upload" />
-                    <label>Load the file you want to analyse:</label>
-                    <input type="file" name="user_file" />
-                </form>
-            </div>
-
-            <button class="container"> COMPARE </button> 
-
         </center>
     </body>
 </html>
